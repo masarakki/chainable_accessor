5a7fb15b2b927bcd80a24a4e5270a1292e6a36cb
masarakki <masaki182@gmail.com>
2021-08-12 23:21:45 +0900

    immutable-support

--- lib/chainable_accessor.rb
+++ lib/chainable_accessor.rb
@@ -3,13 +3,13 @@
 require_relative 'chainable_accessor/version'
 
 module ChainableAccessor
-  def chainable_accessor(*attributes)
+  def chainable_accessor(*attributes, immutable: false)
     mod = Module.new do
       attributes.each do |name|
         define_method name do |val = nil|
           return super() unless val
 
-          tap { |x| x.send("#{name}=", val) }
+          (immutable ? dup : self).tap { |x| x.send("#{name}=", val) }
         end
       end
     end
