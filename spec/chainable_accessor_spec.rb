cfe52692d992ece8a8a603bc20ebdc370d99e7fb
masarakki <masaki182@gmail.com>
2021-08-12 22:49:10 +0900

    implement

--- spec/chainable_accessor_spec.rb
+++ spec/chainable_accessor_spec.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
 RSpec.describe ChainableAccessor do
+  subject(:obj) { klazz.new }
   it { expect(ChainableAccessor::VERSION).to be_a String }
+
+  let(:klazz) do
+    Class.new do
+      extend ChainableAccessor
+      attr_accessor :page, :per, :word
+
+      chainable_accessor :page, :per, :foo
+
+      def foo=(val)
+        @foo = val.to_i
+      end
+
+      def foo
+        @foo.to_s
+      end
+    end
+  end
+
+  it { expect(obj.page(3).per(15)).to eq obj }
+  it { expect(obj.page(3).per(15).page).to eq 3 }
+  it { expect(obj.foo('10').foo).to eq '10' }
 end
